d48c9977186dd992b410d579b67d859ba91b7cfa
dinukasw <dinukasanthush50@gmail.com>
2025-10-19 22:14:11 +0530

    created navbar icon and theme toogle with provider

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import React from "react";
+import { LogOut, Moon, Settings, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ThemeToggleButton } from "./ThemeToogleButton";
+
+const Navbar = () => {
+    return (
+        <nav className="p-4 flex items-center justify-between">
+            {/*LEFT*/}
+            collapseButton
+            {/*RIGHT*/}
+            <div className="flex items-center gap-4">
+                <Link href="/">Dashboard</Link>
+                <ThemeToggleButton />
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent sideOffset={10}>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+                            Profile
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                            <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+                            Settings
+                        </DropdownMenuItem>
+                        <DropdownMenuItem variant="destructive">
+                            <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+                            Logout
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
